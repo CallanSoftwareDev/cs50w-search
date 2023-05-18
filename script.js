84205b4102b8ff5a0030b654db87c5889f646152
CallanSoftwareDev <c.jacka07@gmail.com>
2023-05-19 01:38:39 +0930

    Finished Project 0

--- script.js
+++ script.js
@@ -3,17 +3,54 @@ function changeThemes() {
     const stylesheet = document.getElementById("css");
     const glogof = document.getElementById("glogofull")
     const glogos = document.getElementById("glogo")
-    const text = document.getElementById("themetext")
+    const icon = document.getElementById("tabicon")
     
     if (checkbox.checked) {
-        stylesheet.href = "style.css"; // Replace with the new stylesheet href
+        stylesheet.href = "style.css";
         glogof.src = "Images/glogofull.png"
         glogos.src = "Images/glogo.png"
-        text.innerHTML = "Light Mode"
+        icon.href = "Images/glogo.png"
     } else {
-        stylesheet.href = "style2.css"; // Replace with the new stylesheet href
+        stylesheet.href = "style2.css";
         glogof.src = "Images/glogowfull.png"
         glogos.src = "Images/glogow.png"
-        text.innerHTML = "Dark Mode"
+        icon.href = "Images/glogow.png"
+    }
+}
+
+function imgThemes() {
+    const checkbox = document.getElementById("themes");
+    const stylesheet = document.getElementById("css");
+    const glogof = document.getElementById("glogofull")
+    const glogos = document.getElementById("glogo")
+    const icon = document.getElementById("tabicon")
+    
+    if (checkbox.checked) {
+        stylesheet.href = "style.css";
+        glogof.src = "Images/glogoimgfull.png"
+        glogos.src = "Images/glogo.png"
+        icon.href = "Images/glogo.png"
+    } else {
+        stylesheet.href = "style2.css";
+        glogof.src = "Images/glogoimgwfull.png"
+        glogos.src = "Images/glogow.png"
+        icon.href = "Images/glogow.png"
+    }
+}
+
+function advThemes() {
+    const checkbox = document.getElementById("themes");
+    const stylesheet = document.getElementById("css");
+    const glogos = document.getElementById("glogo")
+    const icon = document.getElementById("tabicon")
+    
+    if (checkbox.checked) {
+        stylesheet.href = "style.css";
+        glogos.src = "Images/glogo.png"
+        icon.href = "Images/glogo.png"
+    } else {
+        stylesheet.href = "style2.css";
+        glogos.src = "Images/glogow.png"
+        icon.href = "Images/glogow.png"
     }
 }
